f93e8b418f54d6b7aa17e2e6b14e31099d411e40
embpgp <pgpcsust@gmail.com>
2021-08-05 07:56:22 +0800

    ret val error

--- leetcode/string_array/binary_search/main.go
+++ leetcode/string_array/binary_search/main.go
@@ -13,17 +13,17 @@ func searchInsert(nums []int, target int) int {
 		}
 		if target < nums[mid] {
 			right = mid - 1
-		}else {
+		} else {
 			left = mid + 1
 		}
 	}
 
-	return -1
+	return left
 
 }
 
-func main(){
-	array := []int{1,3,5,6}
+func main() {
+	array := []int{1, 3, 5, 6}
 	fmt.Println(searchInsert(array, 5))
 
 }

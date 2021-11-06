26d69a0bdc51df120aab3c36c0dd8439a8fa8ff4
embpgp <pgpcsust@gmail.com>
2021-11-06 20:57:29 +0800

    channel close

---
+++ 20/channel_close/channel_close_test.go
@@ -0,0 +1,49 @@
+package channel_close
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func dataProducer(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+		close(ch)
+		ch <- 11
+		wg.Done()
+	}()
+}
+
+func dataReceiver(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for {
+			if data, ok := <-ch; ok {
+				fmt.Println(data)
+			} else {
+				break
+			}
+
+		}
+		wg.Done()
+	}()
+
+}
+
+/*
+ 多少个channel以及数据长度问题需要解决，无法知道
+*/
+
+func TestCloseChannel(t *testing.T) {
+	var wg sync.WaitGroup
+	ch := make(chan int)
+	wg.Add(1)
+	dataProducer(ch, &wg)
+	wg.Add(1)
+	dataReceiver(ch, &wg)
+	wg.Add(1)
+	dataReceiver(ch, &wg)
+	wg.Wait()
+}

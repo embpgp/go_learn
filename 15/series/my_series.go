acabdb131c3e493461a498a90442f78705beb818
embpgp <pgpcsust@gmail.com>
2021-11-06 14:07:52 +0800

    package init

--- 15/series/my_series.go
+++ 15/series/my_series.go
@@ -1,7 +1,18 @@
 package series
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
+func init() {
+	fmt.Println("init 1")
+}
+
+func init() {
+	fmt.Println("init 2")
+
+}
 func GetFibonacci(n int) ([]int, error) {
 	if n < 2 || n > 1000 {
 		return nil, errors.New("n should be in [2, 1000]")

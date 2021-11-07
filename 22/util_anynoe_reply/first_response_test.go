16784a1b7e3cd99eea3f1af6568a5807f7f3e5fb
embpgp <pgpcsust@gmail.com>
2021-11-07 09:19:39 +0800

    all response

--- 22/util_anynoe_reply/first_response_test.go
+++ 22/util_anynoe_reply/first_response_test.go
@@ -14,7 +14,7 @@ func runTask(id int) string {
 
 func FirstResponse() string {
 	numOfRunner := 10
-	ch := make(chan string, numOfRunner-2) //带buffer的ch，只要有buffer，就可以写入后立刻返回，无需阻塞，解耦的
+	ch := make(chan string, numOfRunner-1) //带buffer的ch，只要有buffer，就可以写入后立刻返回，无需阻塞，解耦的
 	for i := 0; i < numOfRunner; i++ {
 		go func(i int) {
 			ret := runTask(i)
@@ -24,9 +24,27 @@ func FirstResponse() string {
 	return <-ch
 }
 
+func AllResponse() string {
+	numOfRunner := 10
+	ch := make(chan string, numOfRunner)
+	for i := 0; i < numOfRunner; i++ {
+		go func(i int) {
+			ret := runTask(i)
+			ch <- ret
+		}(i)
+	}
+	//return <-ch
+	finalRet := ""
+	for j := 0; j < numOfRunner; j++ {
+		finalRet += <-ch + "\n"
+	}
+	return finalRet
+}
+
 func TestFirstResponse(t *testing.T) {
 	t.Log("before:", runtime.NumGoroutine())
 	t.Log(FirstResponse())
+	t.Log(AllResponse())
 	time.Sleep(time.Second * 1)
 	t.Log("after:", runtime.NumGoroutine())
 }

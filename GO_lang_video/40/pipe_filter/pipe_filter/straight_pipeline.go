3919a8f1cb4dfc91d7d2cab3727a19397dbc8a1e
embpgp <pgpcsust@gmail.com>
2021-11-08 15:33:59 +0800

    pipeline

---
+++ GO_lang_video/40/pipe_filter/pipe_filter/straight_pipeline.go
@@ -0,0 +1,25 @@
+package pipe_filter
+
+type StraightPipeline struct {
+	Name    string
+	Filters *[]Filter
+}
+
+func NewStraightPipeline(name string, filter ...Filter) *StraightPipeline {
+	return &StraightPipeline{
+		Name:    name,
+		Filters: &filter,
+	}
+}
+func (f *StraightPipeline) Process(data Request) (Response, error) {
+	var ret interface{}
+	var err error
+	for _, filter := range *f.Filters {
+		ret, err = filter.Process(data)
+		if err != nil {
+			return ret, err
+		}
+		data = ret
+	}
+	return ret, err
+}
